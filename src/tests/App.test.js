44d94a5e0ea27b318451f20ae049b605b1fdb145
GabrielModa <gabriel-moda@hotmail.com>
2021-11-05 16:55:47 -0300

    01, arrumando lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import { screen } from '@testing-library/react'
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
-import userEvent from '@testing-library/user-event';
 
 describe('Testando o Router Dom', () => {
-
-it('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
     const { history } = renderWithRouter(<App />);
-    
-    const home = screen.getByRole('link', {name: /home/i});
+
+    const home = screen.getByRole('link', { name: /home/i });
     const about = screen.getByRole('link', { name: /about/i });
-    const favorites = screen.getByRole('link', { name: /favorite pokémons/i});
-    
+    const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
 
     expect(home).toBeInTheDocument();
-    userEvent.click(home)
+    userEvent.click(home);
     expect(history.location.pathname).toBe('/');
-    
-    userEvent.click(about)
+
+    userEvent.click(about);
     expect(history.location.pathname).toBe('/about');
 
-    userEvent.click(favorites)
+    userEvent.click(favorites);
     expect(history.location.pathname).toBe('/favorites');
 
     history.push('/Not-Found');
-        const headingNotFound = screen.getByRole('img', { name: /crying emoji/i })
-        expect(headingNotFound).toBeInTheDocument()
-    })
-
- });
+    const headingNotFound = screen.getByRole('img', { name: /crying emoji/i });
+    expect(headingNotFound).toBeInTheDocument();
+  });
+});
