64f1f598531e5d13e41065ce6833c82319ae3827
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-11-08 16:27:39 -0300

    a

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from '../renderWithRouter';
+import renderWithRouter from './Utilities/renderWithRouter';
 
 describe('Testando o Router Dom', () => {
   it('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
