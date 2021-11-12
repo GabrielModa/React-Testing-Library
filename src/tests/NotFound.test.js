b47bb2571bb5fba9cf61e6dd3d6e2748896dbe2a
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-11-12 02:22:11 -0300

    04, OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './Utilities/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Teste Page requested not found', () => {
+  it('Teste se página contém um texto Page "requested not found 😭" ', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByRole('heading',
+      { name: /page requested not found crying emoji/i }))
+      .toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    expect(screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i }))
+      .toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
