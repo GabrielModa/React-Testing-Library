0621781c553d40e076fdac1e60d767b0a5770db4
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-11-12 01:47:03 -0300

    03, OK

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -6,8 +6,8 @@ import App from '../App';
 describe('Testa se a página contém as informações sobre a Pokédex', () => {
   it('Teste se a página contém um heading "h2" com o texto "About Pokédex"', () => {
     const { history } = renderWithRouter(<App />);
-
     history.push('/about');
+
     const about = screen.getByRole('heading', { name: /about pokédex/i, level: 2 });
     expect(about).toBeInTheDocument();
 
@@ -17,3 +17,5 @@ describe('Testa se a página contém as informações sobre a Pokédex', () => {
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
+
+// refatorar com expect direto ...
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './Utilities/renderWithRouter';
+import App from '../App';
+
+describe('Testa se é exibido o Pokemon Favorito ou "No favorite pokemon found".', () => {
+  it('Teste se é exibido "No favorite pokemon found".', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+
+    const noFavoriteFound = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavoriteFound).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const detailsPokemon = screen.getByRole('link', { name: /more details/i });
+    expect(detailsPokemon).toBeInTheDocument();
+
+    userEvent.click(detailsPokemon);
+    expect(history.location.pathname).toContain('/pokemons');
+
+    userEvent.click(screen.getByText('Pokémon favoritado?'));
+    expect(screen.getByLabelText('Pokémon favoritado?')).toBeChecked();
+
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favoriteLink);
+    expect(history.location.pathname).toContain('/favorites');
+
+    const starCount = screen.getAllByRole('img', { name: /favorite/i });
+    expect(starCount.length).toBe(1);
+  });
+});
